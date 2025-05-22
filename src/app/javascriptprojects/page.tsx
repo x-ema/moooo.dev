acb7e783f902023fb0ae3286ac3799f1e8a369ed
x-ema <alexmdb0+github@gmail.com>
2025-05-21 21:03:50 -0700

    insert good commit here

--- src/app/javascriptprojects/page.tsx
+++ src/app/javascriptprojects/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
             <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
                 <h1 className="text-3xl font-bold">Projects to learn javascript </h1>
             <ol>
-                <li>calculator</li>
+                <li><a href="javascriptprojects/calculator">calculator</a></li>
                 <li>typing tutor</li>
                 <li>inventory management system</li>
                 <li>helpdesk ticket system</li>
