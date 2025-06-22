659dd83af1bcd421059ba5a76dcef729878d0a6e
x-ema <alexmdb0+github@gmail.com>
2025-06-22 16:37:27 -0700

    UI Refined, added links, homelab stack and list of running servers and game servers

--- README.md
+++ README.md
@@ -1,36 +1,34 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+To Do: 
+-con pics archive
+-list of game servers running + uptime
+-list of services and their links [synctube, etc]
+-fancy webgl stuff
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,386 @@
+import Link from "next/link";
+import "@radix-ui/themes/styles.css"
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-bold animate-spin">
-       moooo.dev
-      </h1>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-items-center sm:items-start">
-        <p>
-          Welcome to moooo.dev, where you can milk my fucking cock!
-        </p>
+    <div className="min-h-screen bg-gradient-to-b from-background via-background to-gray-950">
+      <main className="flex flex-col">
 
-        <div className="mt-[10px]">
-          <h1 className="text-xl font-bold ">todo list:</h1>
-          <ul className="list-disc pl-5 space-y-2">
-            <li>Learn Javascript</li>
-            <li>Learn React</li>
-            <li>Learn Next.js</li>
-          </ul>
-          <br></br>
-          <a href="https://github.com/deepakkumar55/ULTIMATE-JAVASCRIPT-PROJECT/tree/main">deepakkumar55/ULTIMATE-JAVASCRIPT-PROJECT</a><br></br>
-          <a href="/javascriptprojects">Projects for learning Javascript</a>
+        <div className="flex flex-col">
+          <div className="text-6xl content-center my-10 mx-auto font-bold text-gradient animate-fade-in text-transparent bg-clip-text bg-gradient-to-r from-neutral-850 to-neutral-50">moooo.dev</div>
+          <div className="justify-center my-10 mx-auto text-2xl font text-gray-400 leading-relaxed">Welcome to my homelab! Here you&apos;ll find some public services and personal projects I host from home.</div>
         </div>
+        
+
+{/* Start public services section */}
+        <div className="text-3xl mx-auto">Public Servies</div>
+        <div className="grid grid-cols-3 gap-10 justify-between container mx-auto px-6 py-4">
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/minecraft.jpeg)]
+          bg-center
+          ">
+            <h3 className="text-2xl mb-5 z-1 z-2 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Minecraft</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Vanilla Fabirc Minecraft server with cross platform compatibility via Geyser.
+            </p>
+            <div className="border-gray-600 hover:border-white transition-all duration-300 my-5">
+                <Link href={"https://moooo.dev/mods/minecraft"}>
+                <button className="
+                  border border-input bg-background hover:bg-accent hover:text-accent-foreground
+                  h-9 rounded-md px-3
+                  border-gray-600 hover:border-white transition-all duration-300
+                  ">Mods</button>
+                </Link>
+                <Link href={"https://map.moooo.dev"}>
+                <button className="
+                  border border-input bg-background hover:bg-accent hover:text-accent-foreground
+                  h-9 rounded-md px-3
+                  border-gray-600 hover:border-white transition-all duration-300
+                  ">BlueMap</button>
+                </Link>
+                <Link href={""}>
+                <button className="
+                  border border-input bg-background hover:bg-accent hover:text-accent-foreground
+                  h-9 rounded-md px-3
+                  border-gray-600 hover:border-white transition-all duration-300
+                  ">games.moooo.dev</button>
+                </Link>
+              </div>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/factorio.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Factorio</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Heavily modded Factorio space age server. Primarily features Krastorio 2.
+            </p>
+            <Link href={"https://moooo.dev/mods/factorio"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Mods</button>
+            </Link>
+            <Link href={"steam://run/427520//--mp-connect%20games.moooo.dev:34197/"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">games.moooo.dev</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/synctube.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">SyncTube</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Instance of SyncTube. Watch videos and listen to music with your friends. No account required. 
+            </p>
+            <Link href={"https://synctube.moooo.dev"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">SyncTube</button>
+            </Link>
+            <Link href={"https://github.com/RblSb/SyncTube"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+        </div>
+
+{/* Start private services section */}
+        <div className="text-3xl mx-auto">Private Servies</div>
+        <div className="flex justify-between container mx-auto px-6 py-4 grid grid-cols-3 gap-10">
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/open-webui.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Open-Webui</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Open WebUI is an extensible, feature-rich, and user-friendly self-hosted AI platform designed to operate entirely offline.
+            </p>
+            <Link href={"https://github.com/open-webui/open-webui"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/pihole.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">PiHole</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              The Pi-hole® is a DNS sinkhole that protects your devices from unwanted content without installing any client-side software.
+            </p>
+            <Link href={"https://github.com/pi-hole/pi-hole"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/searxng.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">SearXNG</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Privacy-respecting, hackable metasearch engine
+            </p>
+            <Link href={"https://github.com/searxng/searxng"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/nginxproxymanager.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Nginx Proxy Manager</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Reverse proxy with support for SSL using Nginx under the hood.
+            </p>
+            <Link href={"https://github.com/NginxProxyManager/nginx-proxy-manager"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/qbittorrent.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">qBittorent-Nox</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Bittorrent capable of running on a headless server with a remote webui packaged in a ready to deploy Docker container for downloading LinuxISOs.
+            </p>
+            <Link href={"https://github.com/qbittorrent/qBittorrent"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/invidious.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Invidious</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              An open source alternative front-end to YouTube
+            </p>
+            <Link href={"https://github.com/iv-org/invidious"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/plex.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Plex</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Plex is a free app that lets you discover and watch content from various streaming services, your personal media, and live TV.
+            </p>
+            <Link href={"https://plex.tv"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">plex.tv</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/redlib.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Redlib</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              An alternative private front-end to Reddit, with its origins in Libreddit.
+            </p>
+            <Link href={"https://github.com/redlib-org/redlib"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/tailscale.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Tailscale</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Tailscale is a zero configuration VPN that doesn&apos;t go through the public internet.
+            </p>
+            <Link href={"https://tailscale.com/"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">tailscale.com</button>
+            </Link>
+          </div>
+          <div className="
+          bg-gray-900/50
+          p-8 rounded-2xl
+          border border-gray-800
+          hover:border-gray-600
+          transition-all
+          duration-300
+          hover:transform
+          hover:scale-105
+          bg-[url(/glance.png)]
+          bg-center
+          bg-cover
+          ">
+            <h3 className="text-2xl mb-5 drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">Glance</h3>
+            <p className="drop-shadow-[0_1.8px_1.8px_rgba(0,0,0,1)]">
+              Homelab dashboard. &quot;What if you could see everything at a... Glance&quot;
+            </p>
+            <Link href={"https://github.com/glanceapp/glance"}>
+              <button className="
+              border border-input bg-background hover:bg-accent hover:text-accent-foreground
+              h-9 rounded-md px-3
+              border-gray-600 hover:border-white transition-all duration-300 my-5
+              ">Github</button>
+            </Link>
+          </div>
+        </div>
+
+
+      <footer>moooo.dev</footer>
+        
       </main>
     </div>
   );
 }
+
